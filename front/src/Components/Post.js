0bb490a1ec51c6daec6a2f415529ca2c92839bdb
Николаев Александр <99441694+XandrNiko@users.noreply.github.com>
2022-12-25 20:43:46 +0300

    Добавлено отображение специальностей. Переход на страницу профиля.

--- front/src/Components/Post.js
+++ front/src/Components/Post.js
@@ -1,9 +1,8 @@
-import logo from "../icons/hosp1.jpg"
+import { Link } from "react-router-dom";
+import logo from "../icons/hospital_tmp.jpg"
 import Grade from "./Grade";
 
 const Post = ({ post }) => {
-
-    console.log(post)
 
     var addr = <>Не указан</>
     if (post.addr !== undefined) {
@@ -11,19 +10,22 @@ const Post = ({ post }) => {
     }
 
     return (
-        <div className="container post"> 
-            <div className="post-image-container">
-                <img src={logo} className="round post__logo" alt="logo" />    
+        <Link key={post.id} to={`/profile/${post.id}`}>
+            <div className="container post"> 
+                <div className="post-image-container">
+                    <img src={logo} className="round post__logo" alt="logo" />    
+                </div>
+                <div className="container post-info-container">
+                    <article>
+                        <h2>{post.nameFull}</h2>
+                        <p>Адрес: { addr }</p>                    
+                        <p>Рейтинг: </p>
+                        <Grade amount={post.grade}></Grade>
+                        <p>Специальности: {post.specialties && post.specialties.join(", ")}</p>
+                    </article>
+                </div>
             </div>
-            <div className="container post-info-container">
-                <article>
-                    <h2>{post.nameFull}</h2>
-                    <p>Адрес: { addr }</p>                    
-                    <p>Рейтинг: </p>
-                    <Grade amount={post.grade}></Grade>
-                </article>
-            </div>
-        </div>
+        </Link>
     )
 }
 export default Post
